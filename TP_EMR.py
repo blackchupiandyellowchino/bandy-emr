aec75102373a7a6e27148cd9c6e8ca9eba0c2efa
sebamoralesasd <sebasuni96@gmail.com>
2015-08-18 20:57:45 -0300

    Begin code

---
+++ TP_EMR.py
@@ -0,0 +1,42 @@
+import time
+import sys
+
+
+class Tarjeta:
+	def __init__(self):
+		self.arg = arg
+	
+	def payTicket (self):
+
+# saldo - costo
+
+
+	def reload (self):
+
+
+	def money (self):
+
+
+	def doneTravels (self):
+
+
+
+class TarjetaComun (Tarjeta):
+	def __init__ (self):
+
+
+class TarjetaMedioBoleto (Tarjeta):
+	def __init__ (self):
+
+# Sólo válidas de 6 a 24 hs, todos los días (incluyendo sábados y domingos). Meter un if en money()
+
+class bondis:
+	def __init__ (self):
+
+# Empresa, línea, interno 
+
+
+class viaje:
+	def __init__ (self):
+
+# bondi, horario, monto del pasaje
