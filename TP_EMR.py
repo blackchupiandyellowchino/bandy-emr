0707073d91cbe153006287315df58e257c1fe3af
sebamoralesasd <sebasuni96@gmail.com>
2015-08-18 22:35:28 -0300

    Moar

--- TP_EMR.py
+++ TP_EMR.py
@@ -11,13 +11,23 @@ class Tarjeta:
 # saldo - costo
 
 
-	def reload (self):
+	def reload (self,toload):
+		if self.toload == 196:
+			self.guita = self.guita + 230
+		elif self.toload == 368:
+			self.guita = self.guita + 460
+		else
+			self.guita = self.guita + self.toload
 
 
 	def money (self):
+		return self.guita
 
 
 	def doneTravels (self):
+		# if self.bondi_ant == false and Bondi_anterior != Bondi_actual
+
+		# Don't know como representar los bondis
 
 
 
@@ -30,13 +40,19 @@ class TarjetaMedioBoleto (Tarjeta):
 
 # Sólo válidas de 6 a 24 hs, todos los días (incluyendo sábados y domingos). Meter un if en money()
 
-class bondis:
-	def __init__ (self):
+class Bondis:
+	def __init__ (self, empresa, linea, interno):
+		self.emp = empresa
+		self.line = linea
+		self.int = interno
 
 # Empresa, línea, interno 
 
 
-class viaje:
+class Viaje:
 	def __init__ (self):
+		self.cant_viajes = 0
+		list_viajes = []
+
 
 # bondi, horario, monto del pasaje
