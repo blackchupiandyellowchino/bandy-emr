e5ce1f1ccc1a3c4cf8329c976136307d991537a6
sebamoralesasd <sebasuni96@gmail.com>
2015-08-19 22:48:55 -0300

    New stuff

--- TP_EMR.py
+++ TP_EMR.py
@@ -4,11 +4,42 @@ import sys
 
 class Tarjeta:
 	def __init__(self):
-		self.arg = arg
-	
-	def payTicket (self):
+		self.guita = 0
+		self.flag_bondi_ant = False
+		self.bondi_ant = 0
+		aux_doneTravels = Viaje()
+		# Ehm, la lista deberia tener el bondi, la hora y el costo del pasaje de cada viaje
+		list_viajes = []
 
-# saldo - costo
+
+	def payTicket (self,bondiola,horario):
+
+		# guita - costo
+		# Then copiar datos a aux_doneTravels y de ahi a list_viajes[]
+
+		# if tome bondi antes and self.bondi_anterior != Bondi_actual -> Transbordo
+
+			if self.flag_bondi_ant == True and self.bondi_anterior != bondiola.linea:
+				if self.guita >= 1.9:
+					self.guita = self.guita - 1.9
+					self.flag_bondi_ant = False
+					self.bondi_anterior = 0
+					return True
+				else:
+					return False
+
+		# else Normal
+
+			else:
+				if self.guita >= 5.75:
+					self.guita = self.guita - 5.75
+					self.flag_bondi_ant = True
+					self.bondi_anterior = bondiola.linea
+					return True
+				else:
+					return False
+
+
 
 
 	def reload (self,toload):
@@ -25,8 +56,6 @@ class Tarjeta:
 
 
 	def doneTravels (self):
-		# if self.bondi_ant == false and Bondi_anterior != Bondi_actual
-
 		# Don't know como representar los bondis
 
 
@@ -38,7 +67,10 @@ class TarjetaComun (Tarjeta):
 class TarjetaMedioBoleto (Tarjeta):
 	def __init__ (self):
 
-# Sólo válidas de 6 a 24 hs, todos los días (incluyendo sábados y domingos). Meter un if en money()
+# Sólo válidas de 6 a 24 hs, todos los días (incluyendo sábados y domingos). 
+# Meter un if en money()
+
+
 
 class Bondis:
 	def __init__ (self, empresa, linea, interno):
@@ -46,13 +78,15 @@ class Bondis:
 		self.line = linea
 		self.int = interno
 
-# Empresa, línea, interno 
 
 
 class Viaje:
 	def __init__ (self):
 		self.cant_viajes = 0
-		list_viajes = []
+		self.emp = ""
+		self.line = 0
+		self.int = 0
+		self.costo = 0
 
 
 # bondi, horario, monto del pasaje
