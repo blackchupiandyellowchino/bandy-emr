01012b26cfcad7bb97ad28d6ec3f5b14cc46578c
Sebastián Morales <sebasuni96@gmail.com>
2015-09-07 19:46:38 -0300

    Update test_emr.py

--- test_emr.py
+++ test_emr.py
@@ -24,7 +24,7 @@ def test_viajes_done():
 	#Primer viaje -> 230 - 5.75 = 224.25
 	T.payTicket(C116, datetime.strptime ("01/09/2015 18:20", "%d/%m/%Y %H:%M"))
 	#Segundo viaje (transbordo) -> 224.25 - 1.90 = 222.35
-	T.payTicket(C116, datetime.strptime ("01/09/2015 18:40", "%d/%m/%Y %H:%M"))
+	T.payTicket(C112, datetime.strptime ("01/09/2015 18:40", "%d/%m/%Y %H:%M"))
 	assert T.money() == 222.35
 
 	#Tarjeta medio boleto
@@ -34,6 +34,6 @@ def test_viajes_done():
 	#Segundo viaje (transbordo) -> 457.10 - 0.96 = 456.14
 	assert M.money() == 456.14
 	#Tercer viaje (Normal, no medio) -> 456.14 - 5.75 = 450.39
-	M.payTicket(C116, datetime.strptime ("01/09/2015 04:40", "%d/%m/%Y %H:%M"))
+	M.payTicket(C136, datetime.strptime ("01/09/2015 04:40", "%d/%m/%Y %H:%M"))
 	#Cuarto viaje (transbordo normal, no medio) -> 450.39 - 1.9 = 448.49
 	assert M.money() == 448.19
