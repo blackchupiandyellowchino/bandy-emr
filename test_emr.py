b3bef39264922dff532c15c8e53d8b2bd8d0cffa
sebamoralesasd <sebasuni96@gmail.com>
2015-09-08 21:33:26 -0300

    Upd85

--- test_emr.py
+++ test_emr.py
@@ -109,6 +109,7 @@ def test_viajes_done_norm():
 
 
 def test_reload_medio():
+	M1 = TarjetaMedioBoleto()
 	M1.reload(368)
 	M1.reload(50)
 	assert M.money() == 510
@@ -119,7 +120,7 @@ def test_transbordo_medio():
 
 	#Verifica funcionamiento del transbordo
 
-	M1 = Tarjeta()
+	M1 = TarjetaMedioBoleto()
 	M1.reload(10)
 	#Primer viaje -> 10 - 2.90 = 7.10
 	M1.payTicket(C116, datetime.strptime ("03/09/2015 09:30", "%d/%m/%Y %H:%M"))
@@ -134,15 +135,15 @@ def test_3_viajes_norm():
 
 	#Verifica que haya un solo transbordo en tres viajes
 
-	M2 = Tarjeta()
-	M2.reload(30)
-	#Primer viaje -> 30 - 5.75 = 24.25
+	M2 = TarjetaMedioBoleto()
+	M2.reload(20)
+	#Primer viaje -> 20 - 2.90 = 17.10
 	M2.payTicket(C116, datetime.strptime ("01/09/2015 18:20", "%d/%m/%Y %H:%M"))
-	#Segundo viaje (transbordo) -> 24.25 - 1.90 = 22.35
+	#Segundo viaje (transbordo) -> 17.10 - 0.96 = 16.14
 	M2.payTicket(C112, datetime.strptime ("01/09/2015 18:40", "%d/%m/%Y %H:%M"))
-	#Tercer viaje (normal) -> 22.35 - 5.75 = 16.60
+	#Tercer viaje (normal) -> 16.14 - 2.90 = 13.24
 	M2.payTicket(C116, datetime.strptime ("01/09/2015 19:00", "%d/%m/%Y %H:%M"))
-	assert M2.money() == 16.60
+	assert M2.money() == 13.24
 
 
 
@@ -150,13 +151,13 @@ def test_2_viajes_1_bondi():
 
 	#Verifica que no se produzca transbordo en dos viajes realizados con el mismo bondi
 
-	M3 = Tarjeta()
+	M3 = TarjetaMedioBoleto()
 	M3.reload(20)
-	#Primer viaje -> 20 - 5.75 = 14.25
+	#Primer viaje -> 20 - 2.90 = 17.10
 	M3.payTicket(C116, datetime.strptime ("01/09/2015 18:20", "%d/%m/%Y %H:%M"))
-	#Segundo viaje (normal) -> 14.25 - 5.75 = 8.50
+	#Segundo viaje (normal) -> 17.10 - 2.90 = 14.20
 	M3.payTicket(C116, datetime.strptime ("01/09/2015 18:50", "%d/%m/%Y %H:%M"))
-	assert M3.money() == 8.50
+	assert M3.money() == 14.20
 
 
 
@@ -164,17 +165,17 @@ def test_sin_saldo_norm():
 
 	#Verifica que no se pueda realizar un viaje sin saldo en tarjeta
 
-	M4 = Tarjeta()
-	M4.reload(4.50)
+	M4 = TarjetaMedioBoleto()
+	M4.reload(2.00)
 	assert M4.payTicket(C116, datetime.strptime ("01/09/2015 18:20", "%d/%m/%Y %H:%M")) == False
-	assert M4.money() == 4.50
+	assert M4.money() == 2.00
 
 
 
 def test_viajes_done_norm():
 
 
-	M5 = Tarjeta()
+	M5 = TarjetaMedioBoleto()
 	M5.reload(10)
 	M5.payTicket(C116, datetime.strptime ("01/09/2015 18:20", "%d/%m/%Y %H:%M"))
 	M5.payTicket(C136, datetime.strptime ("01/09/2015 18:25", "%d/%m/%Y %H:%M"))
@@ -182,13 +183,13 @@ def test_viajes_done_norm():
 
 
 	assert lista_aux[0].hora == datetime.strptime ("01/09/2015 18:20", "%d/%m/%Y %H:%M")
-	assert lista_aux[0].costo == 5.75
+	assert lista_aux[0].costo == 2.90
 	assert lista_aux[0].emp == "Amarillo"
 	assert lista_aux[0].line == 116
 	assert lista_aux[0].int == 5
 
 	assert lista_aux[1].hora == datetime.strptime ("01/09/2015 18:25", "%d/%m/%Y %H:%M")
-	assert lista_aux[1].costo == 1.90
+	assert lista_aux[1].costo == 0.96
 	assert lista_aux[1].emp == "Naranja"
 	assert lista_aux[1].line == 136
 	assert lista_aux[1].int == 124
